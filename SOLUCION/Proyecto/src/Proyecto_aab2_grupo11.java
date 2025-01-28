055554b25d3d43bfecec52a78822895290b23483
hansalexander <hans06martinez@gmail.com>
2025-01-28 09:56:43 -0500

    ProyectoGrupo11

--- SOLUCION/Proyecto/src/Proyecto_aab2_grupo11.java
+++ SOLUCION/Proyecto/src/Proyecto_aab2_grupo11.java
@@ -6,16 +6,76 @@ import java.util.Scanner;
  * @author hansa
  */
 public class Proyecto_aab2_grupo11 {
+
     public static void main(String[] args) {
-        Scanner tcl = new Scanner(System.in);
-        int lim = 0;
-        String Informacion [] = new String [lim];
-        
+        double[] sueldos = IngresarSueldos();
+        double vivienda = 0, educacion = 0, alimentacion = 0, vestimenta = 0, salud = 0, turismo = 0;
+        ingresarFactura("Vivienda");
+        ingresarFactura("Educacion");
+        ingresarFactura("Alimentacion");
+        ingresarFactura("Vestimenta");
+        ingresarFactura("Salud");
+        ingresarFactura("Turismo");
+
+        double ingresoAnual = calcularIngresoAnual(sueldos);
+        double deducciones = vivienda + educacion + alimentacion + vestimenta + salud + turismo;
+        double impuesto = calcularImpuesto(ingresoAnual, deducciones);
+
+        System.out.println("\nResumen de la Declaracion de Impuestos");
+        System.out.println("Ingreso Anual: " + ingresoAnual);
+        System.out.println("Deducciones Totales: " + deducciones);
+        System.out.println("Base Imponible: " + (ingresoAnual - deducciones));
+        System.out.println("Impuesto a Pagar: " + impuesto);
     }
-    public static void RegistroInformacion (String datos[]){
-        for (int i = 0; i < 10; i++) {
-            
+
+    public static double[] IngresarSueldos() {
+        double[] sueldos = new double[12];
+        double sueldoMensual = solicitarDatoNumerico("Ingrese el sueldo mensual: ");
+        for (int i = 0; i < 12; i++) {
+            sueldos[i] = sueldoMensual;
         }
-        
+        return sueldos;
+    }
+
+    public static double ingresarFactura(String categoria) {
+        return solicitarDatoNumerico("Ingrese el monto de facturas para " + categoria + ": ");
+    }
+
+    public static double calcularIngresoAnual(double[] sueldos) {
+        double total = 0;
+        for (int i = 0; i < sueldos.length; i++) {
+            total += sueldos[i];
+        }
+        return total;
+    }
+
+    public static double calcularImpuesto(double ingresoAnual, double deducciones) {
+        double[] rangosA = {0, 2000, 5000, 1000};
+        double[] porcentaje = {0, 0.05, 0.1, 0.15, 0.2};
+        double ingresoImponible = ingresoAnual - deducciones;
+        double impuesto = 0;
+        for (int i = 0; i < rangosA.length - 1; i++) {
+            if (ingresoImponible > rangosA[i + 1]) {
+                double rangoIngreso = rangosA[i + 1] - rangosA[i];
+                impuesto += rangoIngreso * porcentaje[i + 1];
+            } else if (ingresoImponible > rangosA[i]) {
+                impuesto += (ingresoImponible - rangosA[i]) * porcentaje[i + 1];
+                break;
+            }
+        }
+        return impuesto;
+    }
+
+    public static double solicitarDatoNumerico(String mensaje) {
+        Scanner tcl = new Scanner(System.in);
+        while (true) {
+            System.out.print(mensaje);
+            if (tcl.hasNextDouble()) {
+                return tcl.nextDouble();
+            } else {
+                System.out.println("Ingrese un numero valido.");
+                tcl.next();
+            }
+        }
     }
 }
