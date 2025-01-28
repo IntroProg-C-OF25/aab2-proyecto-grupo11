974e5ddd93ddf76ca94b61740da58915679f78c9
hansalexander <hans06martinez@gmail.com>
2025-01-28 09:54:59 -0500

    ProyectoGrupo11

---
+++ SOLUCION/Proyecto/src/Proyecto_aab2_grupo11.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hansa
+ */
+public class Proyecto_aab2_grupo11 {
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        int lim = 0;
+        String Informacion [] = new String [lim];
+        
+    }
+    public static void RegistroInformacion (String datos[]){
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        
+    }
+}
